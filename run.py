04732926ed7dc43a8c0bb3b2b9c42f54eb45eee3
Granth Agarwal <heygranth@gmail.com>
2025-05-20 08:58:50 +0530

    configured eventlet wsgi instead of gunicorn

--- run.py
+++ run.py
@@ -4,8 +4,11 @@ eventlet.monkey_patch()
 
 
 from app import create_app, socketio
+from eventlet import wsgi
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    # socketio.run(app, debug=True)
+    # Use eventlet's WSGI server to run the app
+    wsgi.server(eventlet.listen(('',10000)), app)
